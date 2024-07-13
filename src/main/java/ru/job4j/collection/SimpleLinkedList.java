c1a85e73ecb69d50489cb868ad34fa733fdd469d
kostas-111 <galkink370@gmail.com>
2024-07-13 21:15:35 +0300

    2. Создать контейнер на базе связанного списка  [#159]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,74 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleLinkedList<E> implements SimpleLinked<E> {
+
+    private int size;
+    private int modCount;
+    private Node<E> head;
+    private Node<E> current;
+
+    @Override
+    public void add(E value) {
+        Node<E> l = current;
+        Node<E> newNode = new Node<>(value, null);
+        current = newNode;
+        if (l == null) {
+            head = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> result = head;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        int expectedModCount = modCount;
+        return new Iterator<E>() {
+
+            private int count = 0;
+            Node<E> l = head;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return count < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                var result = l.item;
+                l = l.next;
+                count++;
+                return result;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
