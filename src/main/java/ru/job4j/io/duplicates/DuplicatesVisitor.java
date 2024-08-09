0dc66e1c63866d4d470ad1da5dd5cb0734d841db
kostas-111 <galkink370@gmail.com>
2024-08-09 08:36:36 +0300

    11. Поиск дубликатов [#315066 #508307]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -17,10 +17,8 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file,
                                      BasicFileAttributes attributes) throws IOException {
-        if (Files.isRegularFile(file)) {
-            FileProperty fp = new FileProperty(Files.size(file), file.getFileName().toString());
-            filesMap.computeIfAbsent(fp, k -> new ArrayList<>()).add(file.toAbsolutePath());
-        }
+        FileProperty fp = new FileProperty(Files.size(file), file.getFileName().toString());
+        filesMap.computeIfAbsent(fp, k -> new ArrayList<>()).add(file.toAbsolutePath());
         return super.visitFile(file, attributes);
     }
 
